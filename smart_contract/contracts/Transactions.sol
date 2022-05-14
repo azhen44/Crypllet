aa6121730952ad257493cefa50a441212e7fb03d
andy zhen <andyzhen44@gmail.com>
2022-05-14 13:49:30 -0700

    deploy smart contract to blockchain

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 
 contract Transactions {
-  uint256 transactionCounter;
+  uint256 transactionCount;
   
   // event to be called
   event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
@@ -19,16 +19,21 @@ contract Transactions {
 
   TransferStruct[] transactions; //transactions will be an array of Transfer Structures
 
-  function addToBlockchain() public {
+  function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
+    transactionCount += 1;
+    transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+
+    //emit transfer
+    emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
 
   }
 
   function getAllTransactions() public view returns (TransferStruct[] memory){
-    //return transactions
+    return transactions;
   }
 
   function getTransactionCount() public view returns (uint256) {
-    // returns # of transaction     
+    return transactionCount;  
   }
 
 }
