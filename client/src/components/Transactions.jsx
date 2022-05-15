dded3c822fe564954d308aeae1d9104d4009ecfc
andy zhen <andyzhen44@gmail.com>
2022-05-14 22:51:29 -0700

    implement transaction history from blockchain

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,6 +1,47 @@
-const Transactions = () => {
+import React, {useContext} from 'react'
+import { TransactionContext } from '../context/TransactionContext';
+import { shortenAdd } from '../utils/shortenAdd';
+
+const TransactionCard = ({addressTo, addressFrom, timestamp, message, keyword, amount, url}) => {
   return (
-    <h1>Transactions</h1>
+    <div className="bg-[#181918] m-4 flex flex-1 2xl:min-w-[450px] 2xl:max-w-[500px] sm:min-w-[270px] sm:max-w-[300px] flex-col p-3 rounded-md hover:shawdow-2xl">
+      <div className="flex flex-col items-center w-full md-3">
+        <div className='w-full mb-6 p-2'>
+          <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer" >
+            <p className='text-white text-base'>From: {shortenAdd(addressFrom)}</p>
+          </a>
+          <a href={`https://ropsten.etherscan.io/address/${addressTo}`} target="_blank" rel="noreferrer" >
+            <p className='text-white text-base'>To: {shortenAdd(addressTo)}</p>
+          </a>
+          <p className='text-white text-base'>Amount: {amount} ETH</p>
+          
+            <p className="text-[#37c7da] font-bold">{timestamp}</p>
+        
+        </div>
+      </div>      
+    </div>
+  )
+
+}
+
+const Transactions = () => {
+  const { currentAccount, transactions} = useContext(TransactionContext)
+  return (
+    <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+      <div className="flex flex-col md:p-12 py-12 px-4">
+        {currentAccount ? (
+          <h3 className="text-white text-3xl text-center my-2">Latest Transactions</h3>
+        ): (
+          <h3 className="text-white text-3xl text-center my-2">Please connect your connect to see Transactions</h3>
+        )}
+
+        <div className="flex flex-wrap justify-center items-center mt-10">
+          {transactions.reverse().map((transaction, i) => (
+            <TransactionCard key={i} {...transaction} />
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -23,9 +23,32 @@ export const TransactionProvider = ({children}) => {
   const [formData, setFromData] = useState({addressTo: '', amount: '', keyword:'', message: ''})
   const [isLoading, setIsLoading] = useState(false)
   const [transactionCount, setTransactionCount] = useState(localStorage.getItem('transactionCount'));
+  const [transactions, setTransactions] = useState([])
+
 
   const handleChange = (e, name) => {
     setFromData((prevState) => ({...prevState, [name]: e.target.value}))
+  }
+
+  const getAllTransactions = async () => {
+    try {
+      if (!ethereum) return alert ("Please install metamask")
+      const transactionContract = getEthereumContract();
+      const availableTransactions = await transactionContract.getAllTransactions();
+      const structuredTransactions = availableTransactions.map((transaction) => ({
+        addressTo: transaction.receiver,
+        addressFrom: transaction.sender,
+        timestamp: new Date(transaction.timestamp.toNumber() * 1000).toLocaleString(),
+        message: transaction.message,
+        keyword: transaction.keyword,
+        amount: parseInt(transaction.amount._hex) / (10 ** 18)
+      }));
+      console.log(structuredTransactions)
+      setTransactions(structuredTransactions)
+    } catch (error) {
+      console.log(error)
+    }
+    
   }
 
   const checkIfWalletIsConnnected = async () => {
@@ -35,7 +58,7 @@ export const TransactionProvider = ({children}) => {
 
       if (accounts.length) {
         setCurrentAccount(accounts[0])
-        // getAllTransactions();
+        getAllTransactions()
         console.log("Connected Wallet: ", accounts[0])
       } else {
         console.log("No accounts found")
@@ -44,6 +67,18 @@ export const TransactionProvider = ({children}) => {
       console.log(error)
       throw new Error("no eth object")      
     }  
+  }
+
+  const checkIfTransactionExist = async () => {
+    try {
+      const transactionContract = getEthereumContract();
+      const transactionCount = await transactionContract.getTransactionCount();
+      window.localStorage.setItem("transactionCount", transactionCount)
+
+    } catch (error) {
+      console.log(error)
+      throw new Error("no eth object")  
+    }
   }
 
   const connectWallet = async () => {
@@ -86,6 +121,7 @@ export const TransactionProvider = ({children}) => {
 
       const transactionCount = await transactionContract.getTransactionCount();
       setTransactionCount(transactionCount.toNumber());
+      window.reload();
 
       
     } catch (error) {
@@ -97,10 +133,11 @@ export const TransactionProvider = ({children}) => {
 
   useEffect(() => {
     checkIfWalletIsConnnected();
+    checkIfTransactionExist();
   },[])
 
   return (
-    <TransactionContext.Provider value={{ connectWallet, currentAccount, formData, setFromData, handleChange, sendTransaction }}>
+    <TransactionContext.Provider value={{ connectWallet, currentAccount, formData, setFromData, handleChange, sendTransaction, transactions, isLoading }}>
       {children}
     </TransactionContext.Provider>
   )
