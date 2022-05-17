5adac8042e01326dfac6f1969e0018a4fd7135bc
andy zhen <andyzhen44@gmail.com>
2022-05-16 19:02:45 -0700

    implement market with myfaveourites tab

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -31,7 +31,7 @@ const Transactions = () => {
     <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
       <div className="flex flex-col md:p-12 py-12 px-4">
         {currentAccount ? (
-          <h3 className="text-white text-3xl text-center my-2">Latest Transactions ({transactionCount})</h3>
+          <h3 className="text-white text-3xl text-center my-2">Latest Transactions</h3>
         ): (
           <h3 className="text-white text-3xl text-center my-2">Please connect your connect to see Transactions</h3>
         )}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import {TransactionProvider} from './context/TransactionContext'
+import { MarketProvider } from './context/MarketContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <TransactionProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <MarketProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </MarketProvider>
   </TransactionProvider>
  
 )
